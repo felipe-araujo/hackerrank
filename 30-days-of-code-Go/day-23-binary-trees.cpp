06293bfffd56c86780c69c5e8516260b4ed4e222
Felipe Araújo <felipe.jra@gmail.com>
2020-03-14 22:21:03 -0300

    Solution to 30 days of code - day 23(written in cpp)

---
+++ 30-days-of-code-Go/day-23-binary-trees.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <cstddef>
+#include <queue>
+#include <string>
+#include <cstdlib>
+
+using namespace std;	
+class Node{
+    public:
+        int data;
+        Node *left,*right;
+        Node(int d){
+            data=d;
+            left=right=NULL;
+        }
+};
+class Solution{
+    public:
+  		Node* insert(Node* root, int data){
+            if(root==NULL){
+                return new Node(data);
+            }
+            else{
+                Node* cur;
+                if(data<=root->data){
+                    cur=insert(root->left,data);
+                    root->left=cur;
+                }
+                else{
+                   cur=insert(root->right,data);
+                   root->right=cur;
+                 }           
+           return root;
+           }
+        }
+
+	void levelOrder(Node * root){
+      //Write your code here
+      if(root==NULL){
+          return;
+      }
+      queue<Node*> nodes;
+      Node *current;
+      nodes.push(root);
+      while(!nodes.empty()){
+          current = nodes.front();
+          std::cout << current->data;
+          std::cout << " ";
+          nodes.pop();
+          if(current->left != NULL){
+              nodes.push(current->left);
+          }
+
+          if(current->right != NULL){
+              nodes.push(current->right);
+          }
+      }
+    }
+
+};//End of Solution
+int main(){
+    Solution myTree;
+    Node* root=NULL;
+    int T,data;
+    cin>>T;
+    while(T-->0){
+        cin>>data;
+        root= myTree.insert(root,data);
+    }
+    myTree.levelOrder(root);
+    return 0;
+}
